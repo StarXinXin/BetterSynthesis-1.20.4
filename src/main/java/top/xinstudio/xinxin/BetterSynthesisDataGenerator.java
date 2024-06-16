c8f33134e29b63756c374d27ee1d9b6451a17e44
Star星辰 <1402832033@qq.com>
2024-06-16 15:44:40 +0800

    修正开源链接地址

--- src/main/java/top/xinstudio/xinxin/BetterSynthesisDataGenerator.java
+++ src/main/java/top/xinstudio/xinxin/BetterSynthesisDataGenerator.java
@@ -2,10 +2,16 @@ package top.xinstudio.xinxin;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import top.xinstudio.xinxin.datagen.ModModelsProvider;
+import top.xinstudio.xinxin.datagen.ModPoiTagsProvider;
 
 public class BetterSynthesisDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModModelsProvider::new);
+		pack.addProvider(ModPoiTagsProvider::new);
 
 	}
 }
