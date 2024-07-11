2aa4a8b511dd86cdfcddd5bd54c53ec3c495e29d
PickStars <1402832033@qq.com>
2024-07-11 23:51:47 +0800

    feat: add gunpowder smelting recipe and modify villager trades- Introduce a new smelting recipe for gunpowder using coals. - Refactor ModTrades to include new trades for villagers, specifically  targeting the CLERIC and FLETCHER professions with updated offers. - Ensure that all changes maintain consistency and improve the overall trading system within the game.
    BREAKING CHANGE: The addition of new recipes and modification of existingtrades may affect existing player configurations or expectations.
    Players will need to adjust their gameplay strategies accordingly.
    
    新增火药冶炼配方，修改村民工种——引进煤制火药冶炼新配方。
    -重构ModTrades，包括村民的新交易，特别是针对牧师和弗莱彻职业的更新报价。
    -确保所有的变化保持一致性，并提高整体
    游戏中的交易系统。
    
    重大改变:添加新配方和修改现有交易可能会影响现有玩家的配置或期望。
    玩家需要相应地调整自己的游戏策略。
    
    Signed-off-by: PickStars <1402832033@qq.com>

--- src/main/java/top/xinstudio/xinxin/util/ModTrades.java
+++ src/main/java/top/xinstudio/xinxin/util/ModTrades.java
@@ -10,7 +10,34 @@ import top.xinstudio.xinxin.villager.ModVillagers;
 
 public class ModTrades {
     public static void registerTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 1, factories -> {
+//        TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 1, factories -> {
+//            factories.add((entity, random) -> new TradeOffer(
+//                    new ItemStack(Items.ROTTEN_FLESH, 42),
+//                    new ItemStack(Items.PHANTOM_MEMBRANE, 1),
+//                    6, 5, 0.05f)
+//            );
+//            factories.add((entity, random) -> new TradeOffer(
+//                    new ItemStack(Items.ROTTEN_FLESH, 22),
+//                    new ItemStack(Items.LEATHER, 1),
+//                    6, 5, 0.05f)
+//            );
+//        });
+
+
+        //为“制箭师”职业的村民注册自定义交易
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 1, factories -> factories.add((entity, random) -> new TradeOffer(
+                new ItemStack(Items.BAMBOO, 20),
+                new ItemStack(Items.EMERALD, 1),
+                6, 5, 0.05f)
+        ));
+
+        //三个交易选项分别允许玩家用10个腐肉兑换1个硬煮鸡蛋，用42个腐肉兑换1个幻影膜，以及用22个腐肉兑换1个皮革
+        TradeOfferHelper.registerVillagerOffers(ModVillagers.BLOCK_BSFURNACE_MASTER, 1, factories -> {
+            factories.add((entity, random) -> new TradeOffer(
+                    new ItemStack(Items.ROTTEN_FLESH, 10),
+                    new ItemStack(ModItems.ITEM_HardboiledEggs, 1),
+                    6, 5, 0.05f)
+            );
             factories.add((entity, random) -> new TradeOffer(
                     new ItemStack(Items.ROTTEN_FLESH, 42),
                     new ItemStack(Items.PHANTOM_MEMBRANE, 1),
@@ -22,11 +49,5 @@ public class ModTrades {
                     6, 5, 0.05f)
             );
         });
-
-        TradeOfferHelper.registerVillagerOffers(ModVillagers.BLOCK_BSFURNACE_MASTER, 1, factories -> factories.add((entity, random) -> new TradeOffer(
-                new ItemStack(Items.ROTTEN_FLESH, 10),
-                new ItemStack(ModItems.ITEM_HardboiledEggs, 1),
-                6, 5, 0.05f)
-        ));
     }
 }
